3f89a65bc539229b30954b163953b64278be4ac0
katyacher <katyacher@inbox.ru>
2023-10-31 12:34:53 +0300

    var std::string text deleted

--- main.cpp
+++ main.cpp
@@ -5,19 +5,16 @@
 int main(int, char**){
     std::cout << "Hello, from 30_2_web_page_title!\n";
 
-    cpr::Response r;
+    cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/html"),
+                    cpr::Header({{"Accept", "text/html"}}));
 
-    r = cpr::Get(cpr::Url("http://httpbin.org/html"),
-                cpr::Header({{"Accept", "text/html"}}));
-
-    std::string text = r.text;
-
-    int title_begin = text.find("<h1>") + 4;
-    int title_end = text.find("</h1>");
+    int title_begin = r.text.find("<h1>") + 4;
+    int title_end = r.text.find("</h1>");
 
     for(int i = title_begin; i < title_end; i++){
-        std::cout << text[i];
+        std::cout << r.text[i];
     }
+    
     std::cout << std::endl;
     return 0;
 }
